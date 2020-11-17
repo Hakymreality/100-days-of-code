e3fc3434b192e98b0f4023990974cdf46446e592
hakym_unltd <clintonadeleke@gmail.com>
2020-11-17 20:59:53 +0100

    Added more Colors

--- Day 1/day1.js
+++ Day 1/day1.js
@@ -1,9 +1,9 @@
 let button = document.getElementById("changeBtn");
 let container = document.getElementById("container")
-let colorArray = ["cadetblue","chatreuse","cornflowerblue", "chocolate","coral","cornsilk","crimson","cyan"];
+let colorArray = ["cadetblue","chatreuse","cornflowerblue", "chocolate","coral","cornsilk","crimson","cyan","tomato","violet","wheat","yellow"];
 function changeColorFn(e) {
     let backgoundColor = container.style.backgroundColor
-    let newColor = Math.round(Math.random() * 7)
+    let newColor = Math.round(Math.random() * 11)
     if (colorArray[newColor] != backgoundColor) {
         container.style.backgroundColor = colorArray[newColor];
     }
