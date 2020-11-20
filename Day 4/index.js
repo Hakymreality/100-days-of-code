ddb9a1fc7df30e0da5e2885ccc8a63a98dd1879e
hakym_unltd <clintonadeleke@gmail.com>
2020-11-20 20:57:20 +0100

    INitial test of JS

--- Day 4/index.js
+++ Day 4/index.js
@@ -3,3 +3,16 @@ let textValue = document.getElementById("textValue");
 let submitBtn = document.getElementById("submitBtn");
 let result = document.getElementById("result");
 
+function getTextValue() {
+    return textValue.value;
+}
+
+function setResultValue() {
+    result.innerText = getTextValue();
+}
+
+function clearResultValue() {
+    result.innerText = ""
+}
+submitBtn.addEventListener("click",setResultValue)
+setTimeout(clearResultValue, 2000);
