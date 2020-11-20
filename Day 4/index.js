a4f1f27f7cabb959403ca510de77361f6e0b918e
hakym_unltd <clintonadeleke@gmail.com>
2020-11-20 20:41:35 +0100

    Began Initial work on JS

---
+++ Day 4/index.js
@@ -0,0 +1,5 @@
+let messageForm = document.getElementsByName("messageForm");
+let textValue = document.getElementById("textValue");
+let submitBtn = document.getElementById("submitBtn");
+let result = document.getElementById("result");
+
