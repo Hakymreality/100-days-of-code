26258db6a281cdce585658332c716f0735100e2f
hakym_unltd <clintonadeleke@gmail.com>
2020-11-21 22:11:35 +0100

    Final Deployment

--- Day 5/index.js
+++ Day 5/index.js
@@ -10,3 +10,9 @@ function removeCount() {
     count ? count--: count = 0;
     countText.innerText = count
 }
+
+let addBtn = document.getElementById("addCountBtn");
+let removeBtn = document.getElementById("removeCountBtn");
+
+addBtn.addEventListener("click",addCount);
+removeBtn.addEventListener("click",removeCount)
