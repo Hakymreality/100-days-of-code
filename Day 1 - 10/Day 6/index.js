8a86d160f32a251c9e77c5e1c2c2493ba0b73261
hakym_unltd <clintonadeleke@gmail.com>
2020-11-22 20:34:59 +0100

    Declared all elements

---
+++ Day 1 - 10/Day 6/index.js
@@ -0,0 +1,11 @@
+let leftControl = document.getElementById("left-control");
+let rightControl = document.getElementById("right-control");
+let image = document.getElementById("imageElem");
+
+let imageArray = [
+  "./Images/code-snippets.svg",
+  "./Images/note-list.png",
+  "./Images/online-page.svg",
+  "./Images/team-preparation.png",
+  "./Images/profile.png",
+];
