d1449c0b72d82ab3251207fe800077d59a0448bd
hakym_unltd <clintonadeleke@gmail.com>
2020-11-23 22:02:52 +0100

    OPtimized Arrays.js

--- Javascript.info/Data Types/arrays.js
+++ Javascript.info/Data Types/arrays.js
@@ -1,13 +1,12 @@
 let styles = ["jazz", "blues"];
 styles.push("rock-n-roll");
 function midIndex(arr) {
-    return (arr.length + 1) / 2 - 1;
+  return (arr.length + 1) / 2 - 1;
 }
 
 let midValue = midIndex(styles);
-styles[midValue] = "classics"
+styles[midValue] = "classics";
 let firstValue = styles.shift();
-console.log(styles);
 styles.shift;
-styles.unshift("rap","reggae");
-console.log(styles)
+styles.unshift("rap", "reggae");
+console.log(styles);
