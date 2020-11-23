2b4a83af969cd35eb83d98822069d8b3f0cee964
hakym_unltd <clintonadeleke@gmail.com>
2020-11-23 22:03:53 +0100

    increased performance in sortByAge algorithm.

--- Javascript.info/Data Types/sortByAge.js
+++ Javascript.info/Data Types/sortByAge.js
@@ -1,5 +1,5 @@
 function sortByAge(arr) {
-  arr.sort((a, b) => (a.age > b.age ? 1 : -1));
+  return arr.sort((a, b) => (a.age > b.age ? 1 : -1));
 }
 let john = { name: "John", age: 25 };
 let pete = { name: "Pete", age: 30 };
