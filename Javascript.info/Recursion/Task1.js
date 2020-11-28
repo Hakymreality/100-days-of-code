541404e44aeab83b9189eb3fa5c9890d3249fb22
hakym_unltd <clintonadeleke@gmail.com>
2020-11-28 11:41:46 +0100

    Tested out all methods

--- Javascript.info/Recursion/Task1.js
+++ Javascript.info/Recursion/Task1.js
@@ -1,8 +1,8 @@
 function sumTo(num) {
-    if (num == 1) {
-        return num
-    } else {
-        return num + sumTo(num - 1)
-    }
+  if (num == 1) {
+    return num;
+  } else {
+    return num + sumTo(num - 1);
+  }
 }
 console.log(sumTo(100));
