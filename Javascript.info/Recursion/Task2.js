174b156b384f3212f1d1a956dfdda45f8f4fdf21
hakym_unltd <clintonadeleke@gmail.com>
2020-11-28 11:45:49 +0100

    Used Terb=nary Operator

--- Javascript.info/Recursion/Task2.js
+++ Javascript.info/Recursion/Task2.js
@@ -1,8 +1,4 @@
 function factorial(n) {
-    if (n == 1) {
-        return n
-    }else{
-        return n * factorial(n - 1);
-    }
+return n != 1 ? n * factorial(n - 1) : 1;
 }
 console.log(factorial(5))
