430b5fce5023b4a9202bffc0e8cb56a98df5933d
hakym_unltd <clintonadeleke@gmail.com>
2020-11-29 13:50:24 +0100

    Completed byFieldJS

---
+++ Javascript.info/Recursion/byField.js
@@ -0,0 +1,14 @@
+function byField(prop) {
+    return function (a,b) {
+        return a.prop > b.prop ? 1 : -1;
+    }
+}
+
+let users = [
+  { name: "Ann", age: 19, surname: "Hathaway" },
+  { name: "John", age: 20, surname: "Johnson" },
+  { name: "Sue", age: 27, surname: "Olive" },
+  { name: "Pete", age: 18, surname: "Peterson" },
+];
+
+console.log(users.sort(byField("age")))
